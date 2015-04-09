14caabaf57dde2b6eee3c237ed0a34ab751f5cea
Benjamin Vickers <benjamin.vickers@interoute.com>
2015-04-09 15:20:19 +0100

    errors in compilation here/think this is due to cloudstack 43/44 discrepency in go-cloudstack generated code

--- builtin/providers/cloudstack/resource_cloudstack_disk.go
+++ builtin/providers/cloudstack/resource_cloudstack_disk.go
@@ -183,7 +183,8 @@ func resourceCloudStackDiskUpdate(d *schema.ResourceData, meta interface{}) erro
 		}
 
 		// Create a new parameter struct
-		p := cs.Volume.NewResizeVolumeParams(d.Id())
+		p := cs.Volume.NewResizeVolumeParams()
+		p.SetId(d.Id())
 
 		// Retrieve the disk_offering UUID
 		diskofferingid, e := retrieveUUID(cs, "disk_offering", d.Get("disk_offering").(string))
